c3f375225359a570980bbdf4f027a49eb7e9d838
ImRTee <imrtee8@gmail.com>
2019-06-07 02:18:35 +1000

    [New] Checkout Page with Contact form

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,9 +19,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Continue to checkout?</p>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <p style={{fontWeight: "bolder"}}>Total price: {props.totalPrice}</p>
+            <p style={{fontWeight: "bolder"}}>Continue to checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Auxilary>
     )
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,31 +78,16 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: false})
     };
     purchaseContinueHandler = () =>{
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: "Raymond",
-        //         address: {
-        //             street: 'Test street 1',
-        //             zipCode: '41352',
-        //             country:"Australia"
-        //         },
-        //         email: 'test@gmail.com'
-        //     },
-        //     deliveryMethod: "fatest"
-        // };
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         console.log(response);
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //         console.log(error)
-        //     })
-        this.props.history.push('/checkout')
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i)+'='+ encodeURIComponent(this.state.ingredients[i]))
+        }
+        queryParams.push('price='+ this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     };
 
 
@@ -131,6 +116,7 @@ class BurgerBuilder extends Component{
             );
             orderSummary = <OrderSummary
                 ingredients={this.state.ingredients}
+                totalPrice={this.state.totalPrice}
                 purchaseCanceled = {this.purchaseCancelHandler}
                 purchaseContinued = {this.purchaseContinueHandler}
             />;
