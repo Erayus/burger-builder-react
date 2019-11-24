f5b5cef99f8cc9ec1d07512ef2463c4b9600415a
Erayus <raymondhieutran@gmail.com>
2019-11-25 00:06:45 +1100

    [New]  handling form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,20 +65,16 @@ class ContactData extends Component {
     orderHandler = ($event) => {
         $event.preventDefault();
         this.setState({loading: true});
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            customer: {
-                name: "Raymond Hieu Tran",
-                address: {
-                    street: 'Test street 1',
-                    zipCode: '41352',
-                    country:"Australia"
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: "fatest"
+            orderData: formData
         };
+
         axios.post('/orders.json', order)
             .then(response => {
                 console.log(response);
@@ -115,7 +111,7 @@ class ContactData extends Component {
 
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                     {formElementsArray.map(formElement => 
                         <Input 
                             key={formElement.id}
@@ -128,7 +124,6 @@ class ContactData extends Component {
                     }
                     <Button 
                         btnType="Success"
-                        clicked = {this.orderHandler}
                     >Order</Button>
                 </form>
         );
