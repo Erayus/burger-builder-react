9d0d17ee9fc3037dc78a29da40f175ea96a48aea
Erayus <raymondhieutran@gmail.com>
2019-11-26 11:08:51 +1100

    [New] Redux install and setup

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,14 +17,7 @@ const INGREDIENT_PRICES = {
 };
 
 class BurgerBuilder extends Component{
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
-        totalPrice: 0,
+    state = { 
         purchasable: false,
         purchasing: false,
         loading: false
