ab846f23292dd551cf9ea573012b6c3155ec7bdf
Erayus <raymondhieutran@gmail.com>
2019-11-30 16:59:39 +1100

    [New] map actions to the order builder

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,7 +6,8 @@ import classes from './ContactData.module.css';
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
-
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from'../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
@@ -108,7 +109,7 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         };
-
+        this.props.onOrderBurger(order);
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
@@ -202,4 +203,11 @@ const mapStateToProps = state => {
         price: state.totalPrice
     }
 }
-export default connect(mapStateToProps)(ContactData);
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    }
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
