a01519ec743764a2c8c72ee84eb622ad6b71b56f
Erayus <raymondhieutran@gmail.com>
2019-12-06 22:53:43 +1100

    [Refactor] Refractored order reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -10,47 +10,34 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            };
+            return updateObject(state, {purchased: false})
         case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            };
+            return updateObject(state, {loading: true})
         case actionTypes.PURCHASE_BURGER_SUCCESS:
             const newOrder  = {
                 ...action.orderData,
                 id: action.orderId,
             }
-            return {
-                ...state,
+            return updateObject(state, {
                 loading: false,
                 orders: state.orders.concat(newOrder),
                 purchased: true
-            };
+            });
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading:  false
-            };
+            return updateObject(state, {loading: false})
         case actionTypes.FETCH_ORDERS_START:
-            return{
-                ...state,
+            return updateObject(state,{
                 loading: true
-            };
+            });
         case actionTypes.FETCH_ORDER_SUCCESS:
-            return {
-                ...state,
+            return updateObject({
                 orders: action.orders,
                 loading: false
-            };
+            });
         case actionTypes.FETCH_ORDER_FAIL:
-            return{
-                ...state,
+            return updateObject({
                 loading: false
-            }
+            })
         default:
             return state;    
     }
