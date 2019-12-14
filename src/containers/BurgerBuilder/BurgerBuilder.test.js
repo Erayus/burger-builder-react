bc5c08be3be27ec85d0ed76fe41b4f764dcef97a
Erayus <raymondhieutran@gmail.com>
2019-12-14 23:59:33 +1100

    [New] Testing BurgerBuilder  Redux

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,11 @@
+import {configure, shallow} from 'enzyme'; 
+import Adapter from 'enzyme-adapter-react-16';
+
+import {BurgerBuilder} from './BurgerBuilder';
+
+configure({adapter: new Adapter()});
+
+
+describe('<BurgerBuilder/>', () => {
+    
+} );
